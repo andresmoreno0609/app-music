0e09ee154f7a31bf8c3afd26453435ba5f4226cf
Andres Moreno <andresmoreno1991609@gmail.com>
2025-07-14 21:56:52 -0500

    clase 14-Julio-2025 (creacion intro)

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 import { StorageService } from '../services/storage.service';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 
 
 @Component({
@@ -11,9 +13,34 @@ import { StorageService } from '../services/storage.service';
   templateUrl: './intro.page.html',
   styleUrls: ['./intro.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton]
+  imports: [IonContent, CommonModule, FormsModule],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class IntroPage implements OnInit {
+  bgFondo = 'var(--bg-color)';
+  titleColor= 'var(--title-color)';
+
+  buttonClass = 'custom-button';
+
+
+  intro = [
+    {
+      image:"/assets/images/intro/slide1.png",
+      title:"Listen Anywhere",
+      description:"Favorite tunes follow, anytime, anywhere. Worry-free offline playback for your journey."
+    },
+    {
+      image:"/assets/images/intro/slide2.png",
+      title:"Playback",
+      description:"Pristine sound quality for absolute clarity in audio playback."
+    },
+    {
+      image:"/assets/images/intro/slide3.png",
+      title:"Music For You",
+      description:"Curated weekly playlists tailored to your music listening history."
+    }
+  ]
+
 
   constructor(private router: Router,private storageService : StorageService) { }
 
