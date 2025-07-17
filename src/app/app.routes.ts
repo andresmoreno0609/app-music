9124843116d4ffa580f90d136299275c186bbf0f
Andres Moreno <andresmoreno1991609@gmail.com>
2025-07-17 16:18:19 -0500

    creacion formulario de registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -20,4 +20,8 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
   },
+  {
+    path: 'register',
+    loadComponent: () => import('./register/register.page').then( m => m.RegisterPage)
+  },
 ];
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule,ReactiveFormsModule,FormBuilder,Validator, FormGroup, Validators, FormControl } from '@angular/forms';
-import { IonicModule, NavController,ToastController  } from '@ionic/angular';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
+import { IonicModule, NavController, ToastController } from '@ionic/angular';
 import { AuthService } from '../services/auth/auth.service';
 import { StorageService } from '../services/storage.service';
 
@@ -10,55 +17,60 @@ import { StorageService } from '../services/storage.service';
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule,ReactiveFormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule],
 })
 export class LoginPage implements OnInit {
   loginForm: FormGroup;
   bgFondo = 'var(--bg-color)';
-  errorMessage: string = "" ;
+  errorMessage: string = '';
 
   validation_messages = {
     email: [
       { type: 'required', message: 'El email es obligatorio.' },
-      { type: 'email', message: 'Debes ingresar un email válido.' }
+      { type: 'email', message: 'Debes ingresar un email válido.' },
     ],
     password: [
       { type: 'required', message: 'La contraseña es obligatoria.' },
-      { type: 'minlength', message: 'La contraseña debe tener al menos 6 caracteres.' }
-    ]
+      {
+        type: 'minlength',
+        message: 'La contraseña debe tener al menos 6 caracteres.',
+      },
+    ],
   };
-  
-  constructor(private fromBuilder: FormBuilder, private authService : AuthService, private navController : NavController,
-    private toastController: ToastController,private storageService : StorageService
-  ) { 
+
+  constructor(
+    private fromBuilder: FormBuilder,
+    private authService: AuthService,
+    private navController: NavController,
+    private toastController: ToastController,
+    private storageService: StorageService
+  ) {
     this.loginForm = this.fromBuilder.group({
-      email: new FormControl('',[Validators.required,Validators.email]),
-      password: new FormControl('',Validators.required)
-    })
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+    });
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   async loginUser(credentials: any) {
-  try {
-    const response = await this.authService.loginUser(credentials);
-    this.errorMessage = "";
-    await this.storageService.set('validateLogin', true);
-    this.navController.navigateForward("/home");
-  } catch (error) {
-    this.presentErrorToast(error as string);
-    console.log("Response", error);
+    try {
+      const response = await this.authService.loginUser(credentials);
+      this.errorMessage = '';
+      await this.storageService.set('validateLogin', true);
+      this.navController.navigateForward('/home');
+    } catch (error) {
+      this.presentErrorToast(error as string);
+      console.log('Response', error);
+    }
   }
-}
   async presentErrorToast(message: string) {
-  const toast = await this.toastController.create({
-    message: message,
-    duration: 4000,
-    position: 'bottom',
-    cssClass: 'toast-error'
-  });
-  await toast.present();
-}
-
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 4000,
+      position: 'bottom',
+      cssClass: 'toast-error',
+    });
+    await toast.present();
+  }
 }
